857195932bcf4d26c43f58d4d59ae27d2c26efa8
Basir Khan <khanbasir5555@gmail.com>
2024-09-18 16:13:49 +0530

    frontend server added

--- apps/reverse-proxy/index.js
+++ apps/reverse-proxy/index.js
@@ -1,1 +1,24 @@
-console.log("reverse proxy starting")
+const express = require('express');
+const httpProxy = require('http-proxy');
+const app = express();
+const port = 8000;
+//ADD ENV
+const BASE_URL= "https://deploylite.s3.ap-south-1.amazonaws.com/__outputs"
+const proxy = httpProxy.createProxy();
+app.use((req,res)=>{
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
+    const resolvesto = `${BASE_URL}/${subdomain}`;
+    return proxy.web(req,res,{target:resolvesto,changeOrigin:true});
+})
+proxy.on('proxyReq',(proxyReq,req,res)=>{
+    const url = req.url;
+    if (url === '/')
+        proxyReq.path += 'index.html'
+})
+proxy.on('error',(err,req,res)=>{
+    res.status(500).send('Something went wrong');
+})
+app.listen(port,()=>{
+    console.log(`Reverse Proxy Server running on port ${port}`);
+})
