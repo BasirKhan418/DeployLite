c8e334c12b6be280a25898d257f7d0e4a61198c1
Basir Khan <khanbasir5555@gmail.com>
2024-09-21 16:47:16 +0530

    nextjs toploader added

--- apps/deploylite/app/layout.tsx
+++ apps/deploylite/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import NextTopLoader from 'nextjs-toploader';
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -28,6 +29,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextTopLoader
+  color="#2299DD"
+  initialPosition={0.08}
+  crawlSpeed={200}
+  height={3}
+  crawl={true}
+  showSpinner={true}
+  easing="ease"
+  speed={200}
+  shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+  template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+  zIndex={1600}
+  showAtBottom={false}
+/>
         {children}
       </body>
     </html>
