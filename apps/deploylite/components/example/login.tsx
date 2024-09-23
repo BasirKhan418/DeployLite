b05c1a0001379cb24335cb7dcf63dd3ba8682eb8
Basir Khan <khanbasir5555@gmail.com>
2024-09-23 12:28:26 +0530

    minor issue fixed

--- apps/deploylite/components/example/login.tsx
+++ apps/deploylite/components/example/login.tsx
@@ -43,7 +43,7 @@ export default function Login() {
     if(result.success){
       toast.success(result.message)
       setTimeout(()=>{
-       router.push('/')
+       window.open("/","_self");
       },2000)
     }
     else{
