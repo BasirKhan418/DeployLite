7b39fda42d926d2ddcc0d78e2403009a0caaa387
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-07-01 00:56:52 +0530

    html,css,js support added

--- apps/api-server/routes/deploy.js
+++ apps/api-server/routes/deploy.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { reactHost } from "../applogics/host/reacthost.js";
+import { frontendHost } from "../applogics/host/frontendhost.js";
 import { Checkip } from "../applogics/enquiry/Checkip.js";
 import { FullStackHost } from "../applogics/host/FullStackHost.js";
 import { Delete } from "../applogics/delete/Delete.js";
@@ -11,6 +12,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/react',reactHost);
+router.post('/frontend',frontendHost);
 router.post('/fullstack',FullStackHost);
 router.post('/checkip',Checkip);
 router.post('/delete',Delete);
