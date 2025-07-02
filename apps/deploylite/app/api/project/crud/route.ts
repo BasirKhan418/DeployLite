fa6e80eec82516b9b9f4c90034a0adcd95d148bf
Aniket Subudhi <aniketsubudhi00@gmail.com>
2025-07-02 21:43:01 +0530

    fix

--- apps/deploylite/app/api/project/crud/route.ts
+++ apps/deploylite/app/api/project/crud/route.ts
@@ -9,7 +9,7 @@ import Deployment from "../../../../../models/Deployment"
 import { cookies } from "next/headers"
 import CryptoJS from "crypto-js"
 import VirtualSpace from "../../../../../models/VirtualSpace"
-import Webbuilder from "../../../../../models/Webbuilder"
+import Webbuilder from "../../../../../models/WebBuilder"
 export const GET = async () => {
     try {
         await ConnectDb();
